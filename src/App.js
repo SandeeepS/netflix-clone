cf51c48828457806a8a70ea38b7d82031f85ba72
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-05-06 16:45:53 +0530

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import "./App.css"
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
 
 function App() {
   return (
     <div className="App">
-         <h1>Hello world</h1>
+        <NavBar />
+        <Banner />
+        <RowPost />
     </div>
   );
 }
